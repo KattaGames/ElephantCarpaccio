b5c5f7cde185062f39e8673ac7cbee8c5737410f
Badre DAHA BELGHITI <badre.daha1@gmail.com>
2019-07-05 14:33:16 +0200

    [Init] First Commit

---
+++ ElephantCarpaccio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ElephantCarpaccio
+//
+//  Created by Badre DAHA BELGHITI on 05/07/2019.
+//  Copyright © 2019 Badre DAHA BELGHITI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
